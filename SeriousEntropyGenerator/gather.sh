c15a190d4a7948fa01c1680a75591a861cfe0e2c
textbrowser <textbrowser@gmail.com>
2023-11-07 14:41:12 -0500

    Gather.

---
+++ SeriousEntropyGenerator/gather.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# Alexis Megas
+
+rm -f data.txt
+
+for i in $(seq 1 100000); do
+    echo "Gathering point $i."
+    value=$(echo | netcat -q 0 192.168.178.85 5000)
+    echo "$i $value" >> data.txt
+done
