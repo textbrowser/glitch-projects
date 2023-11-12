275fa6eeb06c8d9e9b03f0b66ca6789c61c542b5
textbrowser <textbrowser@gmail.com>
2023-11-11 20:50:30 -0500

    Sample data file.

--- SeriousEntropyGenerator/gather.sh
+++ SeriousEntropyGenerator/gather.sh
@@ -6,5 +6,5 @@ rm -f data.txt
 for i in $(seq 1 100000); do
     echo "Gathering point $i."
     value=$(echo | netcat -q 0 192.168.178.85 5000)
-    echo "$i $value" >> data.txt
+    echo "$value" >> data.txt
 done
