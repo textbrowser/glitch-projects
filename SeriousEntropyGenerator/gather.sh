1d21af81b69264f772e4443c521ff943769b88d0
textbrowser <textbrowser@gmail.com>
2023-11-13 21:14:25 -0500

    Ignore errors.

--- SeriousEntropyGenerator/gather.sh
+++ SeriousEntropyGenerator/gather.sh
@@ -9,7 +9,13 @@ fi
 rm -f data.txt
 
 for i in $(seq 1 100000); do
-    echo "Gathering point $i."
-    value=$(echo | socat - TCP4:192.168.178.85:5000)
-    echo "$value" >> data.txt
+    /bin/echo -n "Gathering point $i... "
+    value=$(echo | socat - TCP4:192.168.178.85:5000 2>/dev/null)
+
+    if [ ! -z "$value" ]; then
+        echo "$value" >> data.txt
+	echo "Lovely."
+    else
+	echo "Error!"
+    fi
 done
